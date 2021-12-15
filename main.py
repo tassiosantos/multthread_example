6c39bd97c806fa40c7e2edeb50a90ec7f2eef6c4
Tássio Nascimento Santos <tassiosantos@gmail.com>
2021-12-15 20:21:10 -0300

    projeto feito

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-from time import time
+from time import *
 import numpy as np
+import threading    
 
 from logging import exception
 from os import *
 from datetime import date
 
+from decimal import *
 
-import _thread      
+
+
+
+
+cinetic_energy_array = []
+potential_energy_array = []
+mass = 10
+position0 = 0
+velocity0 = 0
+g = -10
+tempo = []
     
 def iniciar():
 
-    return "null"
-
-    #print("Programa para calcular a energia cinética e potencial de uma particula lançada para cima")
+    print("Programa para calcular a energia cinética e potencial de uma particula lançada para cima")
 
 def novo_cadastro():
     global potential_energy_array
+    global cinetic_energy_array
     global mass 
     global position0
     global velocity0
@@ -23,7 +34,6 @@ def novo_cadastro():
     global tempo
 
     try:
-        tempo = []
         print("Digite a velocidade inicial:")
         velocity0 = int(input())
         
@@ -33,9 +43,10 @@ def novo_cadastro():
         print("Digite a massa do corpo:")
         mass = int(input())
 
-        eventTime = 2*(velocity0/g)
-        for i in np.linspace(0, eventTime, 100):
-                tempo.append(i)
+        eventTime = abs(2*(velocity0/g))
+        for i in np.linspace(0, eventTime, 10000):
+                getcontext().prec = 3
+                tempo.append(round(i, 1))
         
         #print(tempo)
     except:
@@ -43,7 +54,8 @@ def novo_cadastro():
 
 #Função para calcular a altura do corpo
 def calculate_height (g, position0, velocity0, time): 
-    height = position0 + (velocity0*time) + ((g*(time*time))/2)
+    height = position0 + (velocity0*time) + g*round((time**2),2)/2
+    #print(height)
     return height
 #Função para calcular a velocidade do corpo
 def calculate_velocity (g, velocity0, time):
@@ -51,48 +63,36 @@ def calculate_velocity (g, velocity0, time):
     return velocity
 
 #Função para calcular a energia cinética
-def calculate_cinetic_energy(threadName, mass, g, velocity0, time):
+def calculate_cinetic_energy(threadName, mass, g, velocity0, tempo):
     global cinetic_energy_array
-    print(time)
-    for t in time:
+    for t in tempo:
         velocity = calculate_velocity(g, velocity0, t)
-        cinetic_energy = (mass*(velocity*velocity))/2
+        cinetic_energy = (mass*round((velocity**2),2))/2
         cinetic_energy_array.append(cinetic_energy)
-        print(cinetic_energy)
+        print(threadName, " : " ,cinetic_energy)
 
-def calculate_potential_energy(threadName, mass, gravity, height, time):
+#Função para calcular a energia potencial
+def calculate_potential_energy(threadName, mass, gravity, tempo):
     global potential_energy_array
-    for t in time:
-        potential_energy = mass*gravity*height
+    for t in tempo:
+        height = calculate_height(g, 0, velocity0, t) 
+        potential_energy = abs(mass*gravity*height)
         potential_energy_array.append(potential_energy)
+        print(threadName, " : " ,potential_energy)
+
 
 if __name__ == '__main__':
-    global potential_energy_array
-    global cinetic_energy_array
-    global mass 
-    global position0
-    global velocity0
-    global g
-    global tempo
+    
     iniciar()
     
     novo_cadastro()
-    
     try:
-        
-
-        potential_energy_array = []
-        cinetic_energy_array = []
-
-        _thread.start_new_thread(calculate_cinetic_energy, ("Thread-1", mass, g, velocity0, tempo))
+        thread1 = threading.Thread(target = calculate_cinetic_energy, args=("Thread-1", mass, g, velocity0, tempo))
             
-        #thread2 = _thread.start_new_thread(calculate_potential_energy, ("Thread-2", mass, g, calculate_height(g, 0, velocity0, tempo) ) )
-        #thread1.start()
-        #thread2.start()
-
-
+        thread2 = threading.Thread(target = calculate_potential_energy, args=("Thread-2", mass, g, tempo) )
         
-        #potential_energy_array = thread2.join()
-        print(cinetic_energy_array)
+        thread1.start()
+        thread2.start()
+
     except:
         print("deu ruim")
