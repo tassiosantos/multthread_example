92916f0a6c8d45951e85747fe837f12a8cb24acb
Tássio Nascimento Santos <tassiosantos@gmail.com>
2021-12-15 08:17:26 -0300

    first commit

---
+++ cinetic_energy.py
@@ -0,0 +1,3 @@
+def calculate_cinetic_energy(mass, velocity):
+    cinetic_energy = (mass*(velocity*velocity))/2
+    return cinetic_energy
---
+++ potential_energy.py
@@ -0,0 +1,6 @@
+
+
+
+def calculate_potential_energy(mass, gravity, height):
+    potential_energy = mass*gravity*height
+    return potential_energy
